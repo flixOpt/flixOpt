d1687c8451070156b9b139039392b3956933a2d6
FBumann <117816358+FBumann@users.noreply.github.com>
2024-06-04 20:03:12 +0200

    Renamed System.modBox to System.system_model

--- flixOpt/flixPostprocessing.py
+++ flixOpt/flixPostprocessing.py
@@ -591,7 +591,7 @@ class flix_results():
             # # Speicher finden:
             # setOfStorages = set()
             # # for aFlow in self.inputs + self.outputs:
-            # for acomp in self.modBox.es.allMEsOfFirstLayerWithoutFlows:
+            # for acomp in self.system_model.es.allMEsOfFirstLayerWithoutFlows:
             #   if acomp.__class__.__name__ == 'Storage': # nicht schön, da Storage hier noch nicht bekannt
             #     setOfStorages.add(acomp)      
             # for aStorage in setOfStorages:
