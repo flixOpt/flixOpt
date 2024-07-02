1c1ecbc8014f9822fa165cd9d8fd6f34693792bc
FBumann <117816358+FBumann@users.noreply.github.com>
2024-07-02 16:33:15 +0200

    Updated Authors and Organization TU Dresden

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import setup, find_packages
 def read_requirements(file_path):
     with open(file_path, 'r') as file:
         return [line.strip() for line in file if line.strip()]
-#TODO: Change Autor Name and email
+
 setup(
     name='flixOpt',
     version='0.2.0',
     python_requires='>=3.9, <3.12',
-    author='Felix Bumann',
-    author_email='felixbumann387@gmail.com',
+    author='Felix Bumann, Felix Panitz, Peter Stange',
+    author_email='felixbumann387@gmail.com, baumbude@googlemail.com, peter.stange@tu-dresden.de',
+    maintainer='Professur für Gebäudeenergietechnik und Wärmeversorgung (GEWV), TU Dresden',
+    maintainer_email='peter.stange@tu-dresden.de',
     description='Vector based energy and material flow optimization framework in python.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
