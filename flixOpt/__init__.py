89e232449a435d34dc86728bcc2706aa4ec19113
FBumann <117816358+FBumann@users.noreply.github.com>
2024-07-26 17:26:07 +0200

    standard logging_level of package set to "INFO"

--- flixOpt/__init__.py
+++ flixOpt/__init__.py
@@ -10,5 +10,5 @@ from flixOpt.flow_system import FlowSystem
 from flixOpt.calculation import FullCalculation, SegmentedCalculation, AggregatedCalculation
 from flixOpt.interface import InvestParameters, TimeSeriesRaw
 from flixOpt.postprocessing import flix_results
-from flixOpt.core import setup_logging
-setup_logging()
+from flixOpt.core import setup_logging, change_logging_level
+setup_logging('INFO')
