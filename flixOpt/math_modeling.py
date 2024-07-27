5a24f1c52ce887d0f365a7e6d508a5b20cf2c2fc
FBumann <117816358+FBumann@users.noreply.github.com>
2024-07-27 08:56:04 +0200

    Reordering classes

--- flixOpt/math_modeling.py
+++ flixOpt/math_modeling.py
@@ -21,222 +21,6 @@ pyomoEnv = None  # das ist module, das nur bei Bedarf belegt wird
 logger = logging.getLogger('flixOpt')
 
 
-class MathModel:
-    '''
-    Class for equations of the form a_1*x_1 + a_2*x_2 = y
-    x_1 and a_1 can be vectors or scalars.
-
-    Model for adding vector variables and scalars:
-    Allowed summands:
-    - var_vec * factor_vec
-    - var_vec * factor
-    - factor
-    - var * factor
-    - var * factor_vec  # Does this make sense? Is this even implemented?
-    '''
-
-    def __init__(self,
-                 label: str,
-                 modeling_language: Literal['pyomo', 'cvxpy'] = 'pyomo'):
-        self._infos = {}
-        self.label = label
-        self.modeling_language = modeling_language
-
-        self.countComp = 0  # ElementeZähler für Pyomo
-        self.epsilon = 1e-5  #
-
-        self.solver_name: Optional[str] = None
-        self.model = None  # Übergabe später, zumindest für Pyomo notwendig
-        self.variables: List[Variable] = []  # Liste aller Variablen
-        self.eqs: List[Equation] = []  # Liste aller Gleichungen
-        self.ineqs: List[Equation] = []  # Liste aller Ungleichungen
-        self.objective = None  # objective-Function
-        self.objective_result = None  # Ergebnis
-        self.duration = {}  # Laufzeiten
-        self.solver_log = None  # logging und parsen des solver-outputs
-        self.before_values: Dict[str, Numeric] = {}  # before_values, which overwrite inital before values defined in the Elements.
-
-        if self.modeling_language == 'pyomo':
-            global pyomoEnv  # als globale Variable
-            import pyomo.environ as pyomoEnv
-            logger.debug('Loaded pyomo modules')
-            # für den Fall pyomo wird EIN Modell erzeugt, das auch für rollierende Durchlaufe immer wieder genutzt wird.
-            self.model = pyomoEnv.ConcreteModel(name="(Minimalbeispiel)")
-        elif self.modeling_language == 'cvxpy':
-            raise NotImplementedError('Modeling Language cvxpy is not yet implemented')
-        else:
-            raise Exception('not defined for modeling_language' + str(self.modeling_language))
-
-    def to_math_model(self) -> None:
-        t_start = timeit.default_timer()
-        for variable in self.variables:   # Variablen erstellen
-            variable.to_math_model(self)
-        for eq in self.eqs:   # Gleichungen erstellen
-            eq.to_math_model(self)
-        for ineq in self.ineqs:   # Ungleichungen erstellen:
-            ineq.to_math_model(self)
-
-        self.duration['to_math_model'] = round(timeit.default_timer() - t_start, 2)
-
-    @property
-    def nr_of_equations(self) -> int:
-        return len(self.eqs)
-
-    @property
-    def nr_of_single_equations(self) -> int:
-        return sum([eq.nr_of_single_equations for eq in self.eqs])
-
-    @property
-    def nr_of_inequations(self) -> int:
-        return len(self.ineqs)
-
-    @property
-    def nr_of_single_inequations(self) -> int:
-        return sum([eq.nr_of_single_equations for eq in self.ineqs])
-
-    @property
-    def nr_of_variables(self) -> int:
-        return len(self.variables)
-
-    @property
-    def nr_of_single_variables(self) -> int:
-        return sum([var.length for var in self.variables])
-
-
-    def solve(self,
-              mip_gap: float,
-              time_limit_seconds: int,
-              solver_name: Literal['highs', 'gurobi', 'cplex', 'glpk', 'cbc'],
-              solver_output_to_console: bool,
-              logfile_name: str,
-              **solver_opt) -> None:
-        self.solver_name = solver_name
-        t_start = timeit.default_timer()
-        for variable in self.variables:
-            variable.reset_result()  # altes Ergebnis löschen (falls vorhanden)
-        if self.modeling_language == 'pyomo':
-            if solver_name == 'highs':
-              solver = appsi.solvers.Highs()
-            else:
-              solver = pyomoEnv.SolverFactory(solver_name)
-            if solver_name == 'cbc':
-                solver_opt["ratio"] = mip_gap
-                solver_opt["sec"] = time_limit_seconds
-            elif solver_name == 'gurobi':
-                solver_opt["mipgap"] = mip_gap
-                solver_opt["TimeLimit"] = time_limit_seconds
-            elif solver_name == 'cplex':
-                solver_opt["mipgap"] = mip_gap
-                solver_opt["timelimit"] = time_limit_seconds
-                # todo: threads = ? funktioniert das für cplex?
-            elif solver_name == 'glpk':
-                # solver_opt = {} # überschreiben, keine kwargs zulässig
-                # solver_opt["mipgap"] = mip_gap
-                solver_opt['mipgap'] = mip_gap
-            elif solver_name == 'highs':
-                  solver_opt["mip_rel_gap"] = mip_gap
-                  solver_opt["time_limit"] = time_limit_seconds
-                  solver_opt["log_file"]= "results/highs.log"
-                  solver_opt["parallel"] = "on"
-                  solver_opt["presolve"] = "on"
-                  solver_opt["threads"] = 4
-                  solver_opt["output_flag"] = True
-                  solver_opt["log_to_console"] = True
-            # logfile_name = "flixSolverLog.log"
-            if solver_name == 'highs':
-                solver.highs_options=solver_opt
-                self.solver_results = solver.solve(self.model)
-            else:
-                self.solver_results = solver.solve(self.model, options = solver_opt, tee = solver_output_to_console, keepfiles=True, logfile=logfile_name)
-
-            # Log wieder laden:
-            if solver_name == 'highs':
-                pass
-            else:
-                self.solver_log = SolverLog(solver_name, logfile_name)
-                self.solver_log.parse_infos()
-            # Ergebnis Zielfunktion ablegen
-            self.objective_result = self.model.objective.expr()
-
-        else:
-            raise Exception('not defined for modtype ' + self.modeling_language)
-
-        self.duration['solve'] = round(timeit.default_timer() - t_start, 2)
-
-    @property
-    def infos(self) -> Dict:
-        infos = {}
-        infos['Solver'] = self.solver_name
-
-        info_flixModel = {}
-        infos['flixModel'] = info_flixModel
-
-        info_flixModel['no eqs'] = self.nr_of_equations
-        info_flixModel['no eqs single'] = self.nr_of_single_equations
-        info_flixModel['no inEqs'] = self.nr_of_inequations
-        info_flixModel['no inEqs single'] = self.nr_of_single_inequations
-        info_flixModel['no vars'] = self.nr_of_variables
-        info_flixModel['no vars single'] = self.nr_of_single_variables
-        info_flixModel['no vars TS'] = len(self.all_ts_variables)
-
-        if self.solver_log is not None:
-            infos['solver_log'] = self.solver_log.infos
-        return infos
-
-    @property
-    def all_ts_variables(self) -> List:
-        return [variable for variable in self.variables if isinstance(variable, VariableTS)]
-
-    def describe(self) -> str:
-        return (f'no of Eqs   (single): {self.nr_of_equations} ({self.nr_of_single_equations})\n'
-                f'no of InEqs (single): {self.nr_of_inequations} ({self.nr_of_single_inequations})\n'
-                f'no of Vars  (single): {self.nr_of_variables} ({self.nr_of_single_variables})')
-
-    ##############################################################################################
-    ################ pyomo-Spezifisch
-    # alle Pyomo Elemente müssen im model registriert sein, sonst werden sie nicht berücksichtigt
-    def registerPyComp(self, py_comp, aStr='', oldPyCompToOverwrite=None) -> None:
-        # neu erstellen
-        if oldPyCompToOverwrite == None:
-            self.countComp += 1
-            # Komponenten einfach hochzählen, damit eindeutige Namen, d.h. a1_timesteps, a2, a3 ,...
-            # Beispiel:
-            # model.add_component('a1',py_comp) äquivalent zu model.a1 = py_comp
-            self.model.add_component('a' + str(self.countComp) + '_' + aStr, py_comp)  # a1,a2,a3, ...
-        # altes überschreiben:
-        else:
-            self.__overwritePyComp(py_comp, oldPyCompToOverwrite)
-
-            # Komponente löschen:
-
-    def deletePyComp(self, old_py_comp) -> None:
-        aName = self.getPyCompStr(old_py_comp)
-        aNameOfAdditionalComp = aName + '_index'  # sowas wird bei manchen Komponenten als Komponente automatisch mit erzeugt.
-        # sonstige zugehörige Variablen löschen:
-        if aNameOfAdditionalComp in self.model.component_map().keys():
-            self.model.del_component(aNameOfAdditionalComp)
-        self.model.del_component(aName)
-
-        # name of component
-
-    def getPyCompStr(self, aComp) -> str:
-        for key, val in self.model.component_map().iteritems():
-            if aComp == val:
-                return key
-
-    def getPyComp(self, aStr):
-        return self.model.component_map()[aStr]
-
-    # gleichnamige Pyomo-Komponente überschreiben (wenn schon vorhanden, sonst neu)
-    def __overwritePyComp(self, py_comp, old_py_comp) -> None:
-        aName = self.getPyCompStr(old_py_comp)
-        # alles alte löschen:
-        self.deletePyComp(old_py_comp)
-        # überschreiben:
-        self.model.add_component(aName, py_comp)
-
-    ######## Other Modeling Languages
-
 
 class Variable:
     def __init__(self,
@@ -683,6 +467,225 @@ class SumOfSummand(Summand):
         else:
             self._math_expression = sum(self.variable.var[self.indices[j]] * self.factor_vec[j] for j in self.indices)
             return self._math_expression
+
+
+class MathModel:
+    '''
+    Class for equations of the form a_1*x_1 + a_2*x_2 = y
+    x_1 and a_1 can be vectors or scalars.
+
+    Model for adding vector variables and scalars:
+    Allowed summands:
+    - var_vec * factor_vec
+    - var_vec * factor
+    - factor
+    - var * factor
+    - var * factor_vec  # Does this make sense? Is this even implemented?
+    '''
+
+    def __init__(self,
+                 label: str,
+                 modeling_language: Literal['pyomo', 'cvxpy'] = 'pyomo'):
+        self._infos = {}
+        self.label = label
+        self.modeling_language = modeling_language
+
+        self.countComp = 0  # ElementeZähler für Pyomo
+        self.epsilon = 1e-5  #
+
+        self.solver_name: Optional[str] = None
+        self.model = None  # Übergabe später, zumindest für Pyomo notwendig
+        self.variables: List[Variable] = []  # Liste aller Variablen
+        self.eqs: List[Equation] = []  # Liste aller Gleichungen
+        self.ineqs: List[Equation] = []  # Liste aller Ungleichungen
+        self.objective = None  # objective-Function
+        self.objective_result = None  # Ergebnis
+        self.duration = {}  # Laufzeiten
+        self.solver_log = None  # logging und parsen des solver-outputs
+        self.before_values: Dict[str, Numeric] = {}  # before_values, which overwrite inital before values defined in the Elements.
+
+        if self.modeling_language == 'pyomo':
+            global pyomoEnv  # als globale Variable
+            import pyomo.environ as pyomoEnv
+            logger.debug('Loaded pyomo modules')
+            # für den Fall pyomo wird EIN Modell erzeugt, das auch für rollierende Durchlaufe immer wieder genutzt wird.
+            self.model = pyomoEnv.ConcreteModel(name="(Minimalbeispiel)")
+        elif self.modeling_language == 'cvxpy':
+            raise NotImplementedError('Modeling Language cvxpy is not yet implemented')
+        else:
+            raise Exception('not defined for modeling_language' + str(self.modeling_language))
+
+    def to_math_model(self) -> None:
+        t_start = timeit.default_timer()
+        for variable in self.variables:   # Variablen erstellen
+            variable.to_math_model(self)
+        for eq in self.eqs:   # Gleichungen erstellen
+            eq.to_math_model(self)
+        for ineq in self.ineqs:   # Ungleichungen erstellen:
+            ineq.to_math_model(self)
+
+        self.duration['to_math_model'] = round(timeit.default_timer() - t_start, 2)
+
+    @property
+    def nr_of_equations(self) -> int:
+        return len(self.eqs)
+
+    @property
+    def nr_of_single_equations(self) -> int:
+        return sum([eq.nr_of_single_equations for eq in self.eqs])
+
+    @property
+    def nr_of_inequations(self) -> int:
+        return len(self.ineqs)
+
+    @property
+    def nr_of_single_inequations(self) -> int:
+        return sum([eq.nr_of_single_equations for eq in self.ineqs])
+
+    @property
+    def nr_of_variables(self) -> int:
+        return len(self.variables)
+
+    @property
+    def nr_of_single_variables(self) -> int:
+        return sum([var.length for var in self.variables])
+
+
+    def solve(self,
+              mip_gap: float,
+              time_limit_seconds: int,
+              solver_name: Literal['highs', 'gurobi', 'cplex', 'glpk', 'cbc'],
+              solver_output_to_console: bool,
+              logfile_name: str,
+              **solver_opt) -> None:
+        self.solver_name = solver_name
+        t_start = timeit.default_timer()
+        for variable in self.variables:
+            variable.reset_result()  # altes Ergebnis löschen (falls vorhanden)
+        if self.modeling_language == 'pyomo':
+            if solver_name == 'highs':
+              solver = appsi.solvers.Highs()
+            else:
+              solver = pyomoEnv.SolverFactory(solver_name)
+            if solver_name == 'cbc':
+                solver_opt["ratio"] = mip_gap
+                solver_opt["sec"] = time_limit_seconds
+            elif solver_name == 'gurobi':
+                solver_opt["mipgap"] = mip_gap
+                solver_opt["TimeLimit"] = time_limit_seconds
+            elif solver_name == 'cplex':
+                solver_opt["mipgap"] = mip_gap
+                solver_opt["timelimit"] = time_limit_seconds
+                # todo: threads = ? funktioniert das für cplex?
+            elif solver_name == 'glpk':
+                # solver_opt = {} # überschreiben, keine kwargs zulässig
+                # solver_opt["mipgap"] = mip_gap
+                solver_opt['mipgap'] = mip_gap
+            elif solver_name == 'highs':
+                  solver_opt["mip_rel_gap"] = mip_gap
+                  solver_opt["time_limit"] = time_limit_seconds
+                  solver_opt["log_file"]= "results/highs.log"
+                  solver_opt["parallel"] = "on"
+                  solver_opt["presolve"] = "on"
+                  solver_opt["threads"] = 4
+                  solver_opt["output_flag"] = True
+                  solver_opt["log_to_console"] = True
+            # logfile_name = "flixSolverLog.log"
+            if solver_name == 'highs':
+                solver.highs_options=solver_opt
+                self.solver_results = solver.solve(self.model)
+            else:
+                self.solver_results = solver.solve(self.model, options = solver_opt, tee = solver_output_to_console, keepfiles=True, logfile=logfile_name)
+
+            # Log wieder laden:
+            if solver_name == 'highs':
+                pass
+            else:
+                self.solver_log = SolverLog(solver_name, logfile_name)
+                self.solver_log.parse_infos()
+            # Ergebnis Zielfunktion ablegen
+            self.objective_result = self.model.objective.expr()
+
+        else:
+            raise Exception('not defined for modtype ' + self.modeling_language)
+
+        self.duration['solve'] = round(timeit.default_timer() - t_start, 2)
+
+    @property
+    def infos(self) -> Dict:
+        infos = {}
+        infos['Solver'] = self.solver_name
+
+        info_flixModel = {}
+        infos['flixModel'] = info_flixModel
+
+        info_flixModel['no eqs'] = self.nr_of_equations
+        info_flixModel['no eqs single'] = self.nr_of_single_equations
+        info_flixModel['no inEqs'] = self.nr_of_inequations
+        info_flixModel['no inEqs single'] = self.nr_of_single_inequations
+        info_flixModel['no vars'] = self.nr_of_variables
+        info_flixModel['no vars single'] = self.nr_of_single_variables
+        info_flixModel['no vars TS'] = len(self.all_ts_variables)
+
+        if self.solver_log is not None:
+            infos['solver_log'] = self.solver_log.infos
+        return infos
+
+    @property
+    def all_ts_variables(self) -> List:
+        return [variable for variable in self.variables if isinstance(variable, VariableTS)]
+
+    def describe(self) -> str:
+        return (f'no of Eqs   (single): {self.nr_of_equations} ({self.nr_of_single_equations})\n'
+                f'no of InEqs (single): {self.nr_of_inequations} ({self.nr_of_single_inequations})\n'
+                f'no of Vars  (single): {self.nr_of_variables} ({self.nr_of_single_variables})')
+
+    ##############################################################################################
+    ################ pyomo-Spezifisch
+    # alle Pyomo Elemente müssen im model registriert sein, sonst werden sie nicht berücksichtigt
+    def registerPyComp(self, py_comp, aStr='', oldPyCompToOverwrite=None) -> None:
+        # neu erstellen
+        if oldPyCompToOverwrite == None:
+            self.countComp += 1
+            # Komponenten einfach hochzählen, damit eindeutige Namen, d.h. a1_timesteps, a2, a3 ,...
+            # Beispiel:
+            # model.add_component('a1',py_comp) äquivalent zu model.a1 = py_comp
+            self.model.add_component('a' + str(self.countComp) + '_' + aStr, py_comp)  # a1,a2,a3, ...
+        # altes überschreiben:
+        else:
+            self.__overwritePyComp(py_comp, oldPyCompToOverwrite)
+
+            # Komponente löschen:
+
+    def deletePyComp(self, old_py_comp) -> None:
+        aName = self.getPyCompStr(old_py_comp)
+        aNameOfAdditionalComp = aName + '_index'  # sowas wird bei manchen Komponenten als Komponente automatisch mit erzeugt.
+        # sonstige zugehörige Variablen löschen:
+        if aNameOfAdditionalComp in self.model.component_map().keys():
+            self.model.del_component(aNameOfAdditionalComp)
+        self.model.del_component(aName)
+
+        # name of component
+
+    def getPyCompStr(self, aComp) -> str:
+        for key, val in self.model.component_map().iteritems():
+            if aComp == val:
+                return key
+
+    def getPyComp(self, aStr):
+        return self.model.component_map()[aStr]
+
+    # gleichnamige Pyomo-Komponente überschreiben (wenn schon vorhanden, sonst neu)
+    def __overwritePyComp(self, py_comp, old_py_comp) -> None:
+        aName = self.getPyCompStr(old_py_comp)
+        # alles alte löschen:
+        self.deletePyComp(old_py_comp)
+        # überschreiben:
+        self.model.add_component(aName, py_comp)
+
+    ######## Other Modeling Languages
+
+
 
 
 class SolverLog:
