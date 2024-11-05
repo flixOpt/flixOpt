1c1bc8cfac5aa524e97e255efc37ff89a3f757c4
FBumann <117816358+FBumann@users.noreply.github.com>
2024-11-05 15:07:48 +0100

    Reorganizing commons.py

--- flixOpt/commons.py
+++ flixOpt/commons.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
+from .core import setup_logging, change_logging_level, TimeSeriesData
+
 from .elements import Flow, Bus
 from .effects import Effect
 from .components import Source, Sink, SourceAndSink, Storage, LinearConverter
+from . import linear_converters
+
 from .flow_system import FlowSystem
 from .calculation import FullCalculation, SegmentedCalculation, AggregatedCalculation
-from .interface import InvestParameters, OnOffParameters
-from .postprocessing import flix_results
 from . import solvers
-from .core import setup_logging, change_logging_level, TimeSeriesData
+
+from .interface import InvestParameters, OnOffParameters
+from .aggregation import AggregationParameters
+
+from .postprocessing import flix_results
