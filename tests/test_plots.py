58e14d1f075b0340d62a2881eeca8cb8c90feeee
FBumann <117816358+FBumann@users.noreply.github.com>
2024-11-10 21:37:46 +0100

    Added manual tests

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -2,38 +2,53 @@
 """
 Manual test script for plots
 """
+import unittest
+
 import numpy as np
 import pandas as pd
-
 import matplotlib.pyplot as plt
 import plotly
 
 from flixOpt import plotting
 
 
+class TestPlots(unittest.TestCase):
+    def setUp(self):
+        np.random.seed(72)
+
+    @staticmethod
+    def get_sample_data(nr_of_columns: int = 7,
+                        time_steps_per_period: int = 24,
+                        only_pos_or_neg: bool = True,
+                        datetime_index : bool = True,
+                        column_prefix: str = ''):
+        columns = [f"Region {i + 1}{column_prefix}" for i in range(nr_of_columns)]  # More realistic column labels
+
+        if only_pos_or_neg:
+            positive_data = np.abs(np.random.rand(time_steps_per_period, nr_of_columns) * 100)
+            negative_data = -np.abs(np.random.rand(time_steps_per_period, nr_of_columns) * 100)
+            data = pd.DataFrame(np.concatenate([positive_data, negative_data], axis=1),
+                                columns=[f"Region {i + 1}" for i in range(nr_of_columns)] +
+                                        [f"Region {i + 1} Negative" for i in range(nr_of_columns)])
+        else:
+            data = pd.DataFrame(np.random.randn(time_steps_per_period, nr_of_columns) * 50 + 20,
+                                columns=columns)  # Random data with both positive and negative values
+        if datetime_index:
+            data.index = pd.date_range('2023-01-01', periods=time_steps_per_period, freq='h')
+        return data
+
+    def test_bar_plots(self):
+        data = self.get_sample_data()
+        plotly.offline.plot(plotting.with_plotly(data, 'bar'))
+        plotting.with_matplotlib(data, 'bar')
+        plt.show()
+
+    def test_line_plots(self):
+        data = self.get_sample_data()
+        plotly.offline.plot(plotting.with_plotly(data, 'line'))
+        plotting.with_matplotlib(data, 'line')
+        plt.show()
+
+
 if __name__ == '__main__':
-    only_pos_or_neg = False
-    nr_of_periods = 30
-    time_steps_per_period = 24
-    columns = [f"Region {i+1}" for i in range(nr_of_periods)]  # More realistic column labels
-
-    if only_pos_or_neg:
-        positive_data = np.abs(np.random.rand(time_steps_per_period, nr_of_periods) * 100)
-        negative_data = -np.abs(np.random.rand(time_steps_per_period, nr_of_periods) * 100)
-        data = pd.DataFrame(np.concatenate([positive_data, negative_data], axis=1),
-                            columns=[f"Region {i + 1}" for i in range(nr_of_periods)] +
-                                    [f"Region {i + 1} Negative" for i in range(nr_of_periods)])
-    else:
-        data = pd.DataFrame(np.random.randn(time_steps_per_period, nr_of_periods) * 50 + 20, columns=columns)  # Random data with both positive and negative values
-
-    data.index = pd.date_range('2023-01-01', periods=time_steps_per_period, freq='h')
-
-    mode = 'bar'
-    # Plot with Plotly
-    plotly.offline.plot(plotting.with_plotly(data, mode))
-
-    # Plot with Matplotlib
-    plotting.with_matplotlib(data, mode)
-    plt.show()
-
-    print()
+    unittest.main()
