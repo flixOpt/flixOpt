a8f4ea2be4b3f2f70980fbe76c43783674ed961b
FBumann <117816358+FBumann@users.noreply.github.com>
2024-11-18 13:44:49 +0100

    Adopted Behaviour to Storage

--- flixOpt/components.py
+++ flixOpt/components.py
@@ -177,7 +177,7 @@ class Storage(Component):
         """
         # TODO: fixed_relative_chargeState implementieren
         super().__init__(label, inputs=[charging], outputs=[discharging],
-                         prevent_simultaneous_flows=prevent_simultaneous_charge_and_discharge)
+                         prevent_simultaneous_flows=[charging, discharging] if prevent_simultaneous_charge_and_discharge else None)
 
         self.charging = charging
         self.discharging = discharging
