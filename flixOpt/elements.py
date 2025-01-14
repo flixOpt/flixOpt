5f26dfd7a6c8b470b140c447885fc62d5d23fdf3
FBumann <117816358+FBumann@users.noreply.github.com>
2025-01-14 09:33:55 +0100

    BUGFIX in logger message

--- flixOpt/elements.py
+++ flixOpt/elements.py
@@ -249,7 +249,7 @@ class Flow(Element):
             
         if self.size == CONFIG.modeling.BIG and self.fixed_relative_profile is not None:  # Default Size --> Most likely by accident
             logger.warning(f'Flow "{self.label}" has no size assigned, but a "fixed_relative_profile". '
-                           f'The default size is {Config.BIG_M}. As "flow_rate = size * fixed_relative_profile", '
+                           f'The default size is {CONFIG.modeling.BIG}. As "flow_rate = size * fixed_relative_profile", '
                            f'the resulting flow_rate will be very high. To fix this, assign a size to the Flow {self}.')
 
     @property
