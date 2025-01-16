5e6b312587804d65664956c923001dba6d7d3c71
FBumann <117816358+FBumann@users.noreply.github.com>
2025-01-16 10:39:45 +0100

    Use colors in plotting of network

--- flixOpt/plotting.py
+++ flixOpt/plotting.py
@@ -668,20 +668,18 @@ def visualize_network(
             node_id,
             label=node['label'],
             shape={'Bus': 'circle', 'Component': 'box'}[node['class']],
-            color={'Bus': '#393E46', 'Component': '#00ADB5'}[node['class']],
+            color=node['color'],
             title=node['infos'].replace(')', '\n)'),
             font={'size': 14},
         )
 
-    for edge in edge_infos.values():
-        net.add_edge(
-            edge['start'],
-            edge['end'],
-            label=edge['label'],
-            title=edge['infos'].replace(')', '\n)'),
-            font={'color': '#4D4D4D', 'size': 14},
-            color='#222831',
-        )
+    for id, edge in edge_infos.items():
+        net.add_edge(edge['start'],
+                     edge['end'],
+                     label=edge['label'],
+                     title=edge['infos'].replace(')', '\n)'),
+                     font={"color": "#4D4D4D", "size": 14},
+                     color=edge['color'])
 
     # Enhanced physics settings
     net.barnes_hut(central_gravity=0.8, spring_length=50, spring_strength=0.05, gravity=-10000)
