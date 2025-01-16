82568c19d2b1f9bb3cb21d68216621b3a8d0dcbe
FBumann <117816358+FBumann@users.noreply.github.com>
2025-01-16 10:38:06 +0100

    Added colors to FlowSystem

--- flixOpt/flow_system.py
+++ flixOpt/flow_system.py
@@ -11,7 +11,7 @@ import numpy as np
 from . import utils
 from .core import TimeSeries
 from .structure import Element, SystemModel, get_object_infos_as_dict
-from .elements import Bus, Flow, Component
+from .elements import Bus, Flow, Component, MediumColors
 from .effects import Effect, EffectCollection
 
 logger = logging.getLogger('flixOpt')
@@ -21,6 +21,7 @@ class FlowSystem:
     """
     A FlowSystem organizes the high level Elements (Components & Effects).
     """
+    default_color = MediumColors.colors['unknown']
     def __init__(self,
                  time_series: np.ndarray[np.datetime64],
                  last_time_step_hours: Optional[Union[int, float]] = None):
@@ -87,12 +88,15 @@ class FlowSystem:
             element.transform_data()
 
     def network_infos(self) -> Tuple[Dict[str, Dict[str, str]], Dict[str, Dict[str, str]]]:
+        colors = self.colors()
         nodes = {node.label_full: {'label': node.label,
                                    'class': 'Bus' if isinstance(node, Bus) else 'Component',
+                                   'color': colors[node.label_full],
                                    'infos':  node.__str__()}
                  for node in self.components + list(self.all_buses)}
 
         edges = {flow.label_full: {'label': flow.label,
+                                   'color': colors[flow.label_full],
                                    'start': flow.bus.label_full if flow.is_input_in_comp else flow.comp.label_full,
                                    'end': flow.comp.label_full if flow.is_input_in_comp else flow.bus.label_full,
                                    'infos': flow.__str__()}
@@ -238,6 +242,12 @@ class FlowSystem:
     def all_time_series(self) -> List[TimeSeries]:
         return [ts for element in self.all_elements for ts in element.used_time_series]
 
+    def colors(self) -> Dict[str, str]:
+        """Returns a dictionary of colors for all elements in the flow system."""
+        return {element.label_full: element.medium.color
+        if (isinstance(element, (Bus, Flow)) and element.medium is not None)
+        else self.default_color
+                for element in self.all_elements}
 
 def create_datetime_array(start: str,
                           steps: Optional[int] = None,
