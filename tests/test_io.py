2a65b9be1ca5202afd4e59564564f83e9777192d
FBumann <117816358+FBumann@users.noreply.github.com>
2025-03-11 15:49:44 +0100

    Bugfix i import

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,7 +1,7 @@
 from typing import Dict, List, Optional, Union
 
 import pytest
-from conftest import (
+from .conftest import (
     assert_almost_equal_numeric,
     flow_system_base,
     flow_system_long,
